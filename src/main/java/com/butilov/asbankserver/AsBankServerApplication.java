150ee28927f12f0588347768cbd897b71a708bc1
Dmitry Butilov <acebutilov@gmail.com>
2018-05-26 23:33:21 +0300

    Первый коммит

---
+++ src/main/java/com/butilov/asbankserver/AsBankServerApplication.java
@@ -0,0 +1,12 @@
+package com.butilov.asbankserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsBankServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsBankServerApplication.class, args);
+	}
+}
